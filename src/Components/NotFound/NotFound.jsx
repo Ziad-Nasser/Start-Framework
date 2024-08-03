f172ddd4930bd55b7ff6723b7551684e1f0b8788
Ziad-Nasser <ziadnasser177@gmail.com>
2024-08-03 04:05:46 +0300

    done

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import notfound from '../../assets/Error404.png'
 export default function NotFound() {
     return (
-        <section id='notFound' className='h-screen bg-gray-100 pt-16 flex items-center justify-center'>
-            <div className='text-center'>
-                <h1 className='text-gray-800 mb-2 text-3xl font-bold'>Page Not Found ... </h1>
-                <img src={notfound} className='w-1/4 mx-auto' alt="page not found" />
+        <section id='notFound' className='min-h-screen bg-gray-100 pt-16 flex items-center justify-center'>
+            <div className='text-center w-full max-w-4xl px-4'>
+                <img src={notfound} className='w-full max-w-2xl mx-auto' alt="page not found" />
             </div>
         </section>
 
